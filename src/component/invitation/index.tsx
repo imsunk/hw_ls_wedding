3dc9198a97a6cc5311dd240cd55b690ddcb11594
sunk <sunk@mac1234ui-MacBookPro.local>
2026-05-05 17:04:28 +0900

    fix

--- src/component/invitation/index.tsx
+++ src/component/invitation/index.tsx
@@ -40,9 +40,7 @@ export const Invitation = () => {
         <div className="content">햇살이 뜨거울 땐 가려주고,</div>
         <div className="content">비가 오면 우산이 되어주는</div>
         <div className="content">부부가 되겠습니다.</div>
-        <div className="break" />
-        <div className="content">기쁜날 함께 하시어</div>
-        <div className="content">저희의 앞날을 축복해 주세요.</div>
+      
 
         <div className="break" />
 
