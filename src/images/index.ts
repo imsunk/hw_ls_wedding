7d75b2c339568c0333c87d083f414143cdc39fb6
sunk <sunk@mac1234ui-MacBookPro.local>
2026-05-12 22:44:08 +0900

    fix

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,6 +1,9 @@
-import coverImage from "./cover.png"
+import coverImage from "./6.png"
 import image1 from "./1.jpeg"
-import image2 from "./image2.png"
+import image2 from "./2.jpeg"
+import image3 from "./3.jpeg"
+import image4 from "./4.jpeg"
+import image5 from "./5.jpeg"
 
 /**
  * 메인 커버 이미지
@@ -13,4 +16,7 @@ export const COVER_IMAGE = coverImage
 export const GALLERY_IMAGES = [
   image1,
   image2,
+  image3,
+  image4,
+  image5,
 ]
